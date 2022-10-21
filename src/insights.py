e3b7abd113f2e176186e129b76e0176cb0ea1002
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-10-21 13:17:43 -0300

    feat: implementa a função filter_by_salary_range

--- src/insights.py
+++ src/insights.py
@@ -95,18 +95,12 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
+    list_salary_range = []
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                list_salary_range.append(job)
+        except ValueError:
+            print("Something went wrong")
+    return list_salary_range
