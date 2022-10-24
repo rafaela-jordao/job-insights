8355facb73e8694059fde97bbb8ff56f40ba74b2
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-10-24 11:25:26 -0300

    feat: implementa teste para a função sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,25 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+mock_jobs = [
+    {
+        "min_salary": 5000,
+        "max_salary": 8000,
+        "date_posted": "2020-04-08",
+    },
+    {
+        "min_salary": 2000,
+        "max_salary": 4000,
+        "date_posted": "2020-05-08",
+    }
+]
+
+order_salary_min = [mock_jobs[1], mock_jobs[0]]
+order_salary_max = [mock_jobs[0], mock_jobs[1]]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(mock_jobs, "min_salary")
+    assert mock_jobs == order_salary_min
+
+    sort_by(mock_jobs, "max_salary")
+    assert mock_jobs == order_salary_max
